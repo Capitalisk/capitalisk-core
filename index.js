8faddf8af968b2deaf25a8b9fe1f76a8f33d5b69
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-11 12:16:19 +0100

    v1.0.1

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const path = require('path');
-const LDEM = require('./index');
+const LDEM = require('ldem');
 const objectAssignDeep = require('object-assign-deep');
 const argv = require('minimist')(process.argv.slice(2));
-const defaultConfig = require('./config.json');
-
 const CWD = process.cwd();
+const defaultConfig = require('./config.json');
 
 let config;
 
 if (argv.c) {
   const CUSTOM_CONFIG_PATH = path.join(CWD, argv.c);
-  const customConfig = require(CUSTOM_CONFIG_PATH);
-  config = objectAssignDeep({}, defaultConfig, customConfig);
+  try {
+    const customConfig = require(CUSTOM_CONFIG_PATH);
+    config = objectAssignDeep({}, defaultConfig, customConfig);
+  } catch (error) {
+    throw new Error(
+      `Failed to use config at path ${CUSTOM_CONFIG_PATH} because of error: ${error.message}`
+    );
+  }
 } else {
   config = defaultConfig;
 }
