d833ae4ea2fc22ea61f809d2c267c5ca7df59a74
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-03-15 23:03:51 +0100

    Add support for update scheduling mechanism

--- index.js
+++ index.js
@@ -1,26 +1,86 @@
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const unlink = util.promisify(fs.unlink);
 const LDEM = require('ldem');
 const objectAssignDeep = require('object-assign-deep');
 const argv = require('minimist')(process.argv.slice(2));
+
 const CWD = process.cwd();
-const defaultConfig = require('./config.json');
+const CONFIG_PATH = argv.c ? path.resolve(CWD, argv.c) : path.resolve(__dirname, 'config.json');
+const CONFIG_UPDATES_DIR_PATH = argv.u ? path.resolve(CWD, argv.u) : path.resolve(__dirname, 'updates');
 
 let config;
 
-if (argv.c) {
-  const CUSTOM_CONFIG_PATH = path.join(CWD, argv.c);
-  try {
-    const customConfig = require(CUSTOM_CONFIG_PATH);
-    config = objectAssignDeep({}, defaultConfig, customConfig);
-  } catch (error) {
-    throw new Error(
-      `Failed to use config at path ${CUSTOM_CONFIG_PATH} because of error: ${error.message}`
-    );
-  }
-} else {
-  config = defaultConfig;
+try {
+  config = require(CONFIG_PATH);
+} catch (error) {
+  throw new Error(
+    `Failed to use config at path ${CONFIG_PATH} because of error: ${error.message}`
+  );
 }
 
-new LDEM({
-  config
-});
+(async () => {
+  let configUpdates;
+  let updateFilePaths = {};
+
+  try {
+    let allFiles = await readdir(CONFIG_UPDATES_DIR_PATH);
+    let jsonFileRegex = /\.json$/;
+    let configUpdateFiles = allFiles.filter(fileName => jsonFileRegex.test(fileName));
+    configUpdates = await Promise.all(
+      configUpdateFiles.map(async (fileName) => {
+        let filePath = path.resolve(CONFIG_UPDATES_DIR_PATH, fileName);
+        let content = await readFile(filePath, {encoding: 'utf8'});
+        let update = JSON.parse(content);
+        updateFilePaths[update.id] = filePath;
+        return update;
+      })
+    );
+  } catch (error) {
+    throw new Error(
+      `Failed to load config updates from the ${
+        CONFIG_UPDATES_DIR_PATH
+      } directory because of error: ${
+        error.message
+      }`
+    );
+  }
+
+  let ldem = new LDEM({
+    config,
+    configUpdates
+  });
+
+  (async () => {
+    for await (let {moduleAlias, updates, updatedModuleConfig} of ldem.listener('moduleUpdates')) {
+      config.modules[moduleAlias] = updatedModuleConfig;
+      try {
+        await writeFile(CONFIG_PATH, JSON.stringify(config, ' ', 2));
+      } catch (err) {
+        ldem.logger.fatal(
+          `Failed to write update to config file at path ${CONFIG_PATH} because of error: ${err.message}`
+        );
+        process.exit(1);
+      }
+      await Promise.all(
+        updates.map(async (update) => {
+          let filePath = updateFilePaths[update.id];
+          if (filePath) {
+            try {
+              await unlink(filePath);
+            } catch (err) {
+              ldem.logger.error(
+                `Failed to delete old config update file at path ${filePath} because of error: ${err.message}`
+              );
+            }
+          }
+        })
+      );
+    }
+  })();
+
+})();
---
+++ updates/README.md
@@ -0,0 +1,2 @@
+JSON update files (with a `.json` file extension) can be added to this directory to schedule automatic updates for some time in the future.
+This allows network participants to prepare their nodes for updates ahead of time so that they don't have to be physically present when the update is due to kick in.
